4aa10999618b5645257fffa27ea213310aebc720
Tenzkie10 <kirstengalam455@gmail.com>
2024-05-15 12:29:30 +0800

    with epeks

--- stepbigbrew/src/components/menu.jsx
+++ stepbigbrew/src/components/menu.jsx
@@ -1,4 +1,14 @@
 import m_img1 from '../images/black_coffee.jpg'
+import m_img2 from '../images/caf_latte.jpg'
+import m_img3 from '../images/caffemocha.jpg'
+import m_img4 from '../images/capc.jpg'
+import m_img5 from '../images/car_mac.jpg'
+import m_img6 from '../images/espreconpan.jpg'
+import m_img7 from '../images/espremachi.jpg'
+import m_img8 from '../images/esspresso.jpg'
+import m_img9 from '../images/flatwhite.jpg'
+import m_img10 from '../images/icecafam.jpg'
+import m_img11 from '../images/whitechocmo.jpg'
 
 export default function menu(){
     return (
@@ -6,9 +16,49 @@ export default function menu(){
         <div className="menu-page lg:flex-row">
             <div className="menu-text"><b>MENU</b></div>
                 <div className='coffee-container'>
-                    <label id='bcoffee'>
-                        <img className='' src={m_img1} alt='black coffe'></img>
+                    <label className='coffee-label' id='bcoffee'>
+                        <img src={m_img1} alt='black coffee'></img>
                         Black Coffee
+                    </label>
+                    <label className='coffee-label' id='clatte'>
+                        <img src={m_img2} alt='caffe latte'></img>
+                        Caffe Latte
+                    </label>
+                    <label className='coffee-label' id='cmocha'>
+                        <img src={m_img3} alt='caffee mocha'></img>
+                        Caffee Mocha
+                    </label>
+                    <label className='coffee-label' id='cchino'>
+                        <img src={m_img4} alt='capochino'></img>
+                        Capochino
+                    </label>
+                    <label className='coffee-label' id='cmacch'>
+                        <img src={m_img5} alt='caramel macchiato'></img>
+                        Caramel Macchiato
+                    </label>
+                    <label className='coffee-label' id='econpan'>
+                        <img src={m_img6} alt='espressoconpanna'></img>
+                        Espresso con panna
+                    </label>
+                    <label className='coffee-label' id='emacchi'>
+                        <img src={m_img7} alt='espressomacchiato'></img>
+                        Espresso Macchiato
+                    </label>
+                    <label className='coffee-label' id='espre'>
+                        <img src={m_img8} alt='espresso'></img>
+                        Espresso
+                    </label>
+                    <label className='coffee-label' id='fwhite'>
+                        <img src={m_img9} alt='flatwhite'></img>
+                        Flate White
+                    </label>
+                    <label className='coffee-label' id='icame'>
+                        <img src={m_img10} alt='coldbrewcoffee'></img>
+                        Iced Caffe Americano
+                    </label>
+                    <label className='coffee-label' id='wcmo'>
+                        <img src={m_img11} alt='whitechocolatemocha'></img>
+                        White Chocolate Mocha
                     </label>
                 </div>
         </div>
